e1ccc634f60f9c1e573b168843579607468ca163
Tony LaForgia <alforgia428@gmail.com>
2022-02-21 15:43:29 -0600

    Added Routes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    alert("Ya clicked me!");
+    console.log("History is", history);
+    history.push("/animals");
+  };
   return (
     <div>
       <h1>HOME</h1>
-      <img src="https://pixy.org/download/4718707/" width={500}/>
+      <button onClick={handleClick}>Let's Go</button>
+      <img src="https://pixy.org/download/4718707/" width={500} />
     </div>
   );
 }
